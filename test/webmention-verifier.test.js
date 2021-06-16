433e96c4ffaa7d545de6da8424fbc71cd488faf7
Tim Culverhouse <tim@timculverhouse.com>
2021-06-16 13:52:18 -0500

    a bunch of tests and updates added

--- test/webmention-verifier.test.js
+++ test/webmention-verifier.test.js
@@ -1,5 +1,7 @@
 var assert = require('assert');
 const verifier = require('../lib/webmention-verifier');
+const fs = require('fs');
+const nock = require('nock');
 
 
 describe('webmention-verifier', function() {
@@ -28,13 +30,42 @@ describe('webmention-verifier', function() {
     assert.deepEqual(actual,expected);
   });
 
-  it('should return status code 400 if the target domain cannot be fetched (does not exist)', async function() {
-    const expected={statusCode: 400,body: "Target domain does not exist",};
-    const actual = await verifier('https://www.duckduckgo.com','https://www.thisisavalidurlasdfasdfasd21f25sa1d56fasd.com');
+  it('should return status code 400 if the source url cannot be fetched (does not exist)', async function() {
+    const expected={statusCode: 400,body: "Source url does not exist",};
+    const actual = await verifier('https://www.thisisavalidurlasdfasdfasd21f25sa1d56fasd.com','https://www.duckduckgo.com');
     assert.deepEqual(actual,expected);
   });
 
-//test for jf2 children array
+  it('should return status code 400 if source does not mention target', async function() {
+    const data = fs.readFileSync('./test/data/webmention-verifier-multiple-h-entry.html')
+    const scope = nock('https://www.example.com')
+      .get('/post.html')
+      .reply(200, data)
+
+    const actual = await verifier('https://www.example.com/post.html','https://www.notmentioned.com');
+    assert.equal(actual.statusCode,400)
+  });
+  it('should find in-reply-to', async function() {
+    const data = fs.readFileSync('./test/data/webmention-verifier-single-h-entry.html')
+    const scope = nock('https://www.example.com')
+      .get('/post.html')
+      .reply(200, data)
+
+    const actual = await verifier('https://www.example.com/post.html','https://www.duckduckgo.com');
+    assert.equal(actual["in-reply-to"],"https://www.duckduckgo.com")
+  });
+
+  it('should find in-reply-to with multiple h-entries', async function() {
+    const data = fs.readFileSync('./test/data/webmention-verifier-multiple-h-entry.html')
+    const scope = nock('https://www.example.com')
+      .get('/post.html')
+      .reply(200, data)
+
+    const actual = await verifier('https://www.example.com/post.html','https://www.duckduckgo.com');
+    assert.equal(actual["in-reply-to"],"https://www.duckduckgo.com")
+  });
+
+// test for jf2 children array
 // test for jf2 card in array
 // test(s?) for adding author if h-card not there
 
