efc7775c43342b2f89eeeea5115319199610e3b8
Tim Culverhouse <tim@timculverhouse.com>
2021-06-18 13:37:35 -0500

    representative-h-card algorithm complete

--- test/representative-h-card.test.js
+++ test/representative-h-card.test.js
@@ -13,15 +13,16 @@ describe('representative-h-card()', function() {
 	    "rels": {},
 	    "rel-urls": {}
 	};
-    assert.equal(hcard(data),false);
+    assert.equal(hcard(data,''),false);
   });
 
-  it('should return two h-cards if two h-card are sent, nested', function() {
+  it('should return one h-card if two h-card are found (one is nested) with url and uid on the first one', function() {
     const data = {
     "items": [{
         "type": ["h-card"],
         "properties": {
             "url": ["http://blog.lizardwrangler.com/"],
+            "uid": ["http://blog.lizardwrangler.com/"],
             "name": ["Mitchell Baker"],
             "org": [{
                 "value": "Mozilla Foundation",
@@ -36,11 +37,87 @@ describe('representative-h-card()', function() {
     "rels": {},
     "rel-urls": {}
 		};
-    console.log(hcard(data));
-    assert.deepEqual(hcard(data),data.items[0]);
+    assert.deepEqual(hcard(data,"http://blog.lizardwrangler.com/"),data.items[0]);
+  });
+
+    it('should return one h-card if two h-card are found (one is nested) with url and uid on the second one', function() {
+    const data = {
+    "items": [{
+        "type": ["h-card"],
+        "properties": {
+            "url": ["http://blog.lizardwrangler.com/"],
+            "uid": ["http://blog.lizardwrangler.com/"],
+            "name": ["Mitchell Baker"],
+            "org": [{
+                "value": "Mozilla Foundation",
+                "type": ["h-card"],
+                "properties": {
+                    "name": ["Mozilla Foundation"],
+                    "url": ["http://mozilla.org/"],
+                    "uid": ["http://mozilla.org/"]
+                }
+            }]
+        }
+    }],
+    "rels": {},
+    "rel-urls": {}
+    };
+    assert.deepEqual(hcard(data,"http://mozilla.org/"),data.items[0].properties.org[0]);
   });
   
-  it('should return one h-card if one h-card is sent, nested', function() {
+  it('should return one (nested) h-card with url and uid', function() {
+    const data = {
+    "items": [{
+      "type": [
+        "h-entry"
+      ],
+      "properties": {
+        "published": [
+          "2021-06-05 20:18:36+0000"
+        ],
+        "url": [
+          "https://www.timculverhouse.com/notes/2021-06-05-151836.html"
+        ],
+        "author": [
+          {
+            "value": "Tim Culverhouse",
+            "type": [
+              "h-card"
+            ],
+            "properties": {
+              "name": [
+                "Tim Culverhouse"
+              ],
+              "photo": [
+                "https://www.timculverhouse.com/assets/img/tim-avatar.jpeg"
+              ],
+              "url": [
+                "https://www.timculverhouse.com"
+              ],
+              "uid": [
+                "https://www.timculverhouse.com"
+              ]
+            }
+          }
+        ],
+        "name": [
+          "\"Culverhouse Island\"\n Culverhouse Island"
+        ],
+        "content": [
+          {
+            "value": "\"Culverhouse Island\"",
+            "html": "<p>\"Culverhouse Island\"\n<img src=\"https://www.timculverhouse.com/assets/img/IMG_0719.jpeg\" alt=\"Culverhouse Island\"></p>\n"
+          }
+        ]
+      }
+    }],
+    "rels": {},
+    "rel-urls": {}
+		};
+    assert.deepEqual(hcard(data, 'https://www.timculverhouse.com'),data.items[0].properties.author[0]);
+  });
+
+  it('should return one (nested) h-card with url and rel-me', function() {
     const data = {
     "items": [{
       "type": [
@@ -83,10 +160,38 @@ describe('representative-h-card()', function() {
         ]
       }
     }],
+    "rels": {
+      "me": [
+                "https://www.timculverhouse.com"
+              ]
+    },
+    "rel-urls": {}
+    };
+    assert.deepEqual(hcard(data, 'https://www.timculverhouse.com'),data.items[0].properties.author[0]);
+  });
+
+  it('should return one h-card if two h-card are found (one is nested) with url on the first one which matches target', function() {
+    const data = {
+    "items": [{
+        "type": ["h-card"],
+        "properties": {
+            "url": ["http://blog.lizardwrangler.com/"],
+            "uid": ["http://blog.lizardwrangler.com/"],
+            "name": ["Mitchell Baker"],
+            "org": [{
+                "value": "Mozilla Foundation",
+                "type": ["h-card"],
+                "properties": {
+                    "name": ["Mozilla Foundation"],
+                    "url": ["http://mozilla.org/"]
+                }
+            }]
+        }
+    }],
     "rels": {},
     "rel-urls": {}
-		};
-    assert.deepEqual(hcard(data),data.items[0].properties.author[0]);
+    };
+    assert.deepEqual(hcard(data,"http://blog.lizardwrangler.com/"),data.items[0]);
   });
 
 });
