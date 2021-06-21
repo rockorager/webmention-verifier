00c079f65a294f726f561423913ce5aa91bdb4a4
Tim Culverhouse <tim@timculverhouse.com>
2021-06-21 13:47:05 -0500

    documentation updates

--- README.md
+++ README.md
@@ -5,9 +5,52 @@ A Node.js webmention verifier
 
 ## Installation
 
-```
-> npm install webmention-receiver
+```bash
+npm install webmention-verifier
 ```
 
 ## Usage    
 
+Webmention Verifier will mostly verify webmentions per the [W3c specification](https://www.w3.org/TR/webmention/). Mostly.
+
+```js
+const wmverifier = require('webmention-verifier');
+
+const webmention = wmverifier(source, target[, acceptableHost])
+```
+
+It **will**:
+✅ Verify that `source` and `target` are valid URLs ([must](https://www.w3.org/TR/webmention/#request-verification-p-1))
+✅ Reject the request if `source` and `target` are not the same URL ([must](https://www.w3.org/TR/webmention/#request-verification-p-2))
+✅ Perform a `GET` request on `source` **and** confirm `source` mentions `target` ([must](https://www.w3.org/TR/webmention/#request-verification-p-2))
+✅ Handle arbitrary microformats (IE `<a class="u-arbitrary" href="http://your-site.com>`)
+
+
+It **can**: 
+✅ Check if receiver is a valid resource to accept webmentions for `target` ([should](https://www.w3.org/TR/webmention/#request-verification-p-3))
+
+It **wont**:
+❌ Process the request asynchronously (more below) ([should](https://www.w3.org/TR/webmention/#receiving-webmentions-p-1))
+❌ Return a status code for success (your code needs to handle this part)
+❌ Limit the number of redirects during the `GET` request on `source` ([should](https://www.w3.org/TR/webmention/#webmention-verification-p-3))
+❌ Store the webmention anywhere
+❌ Check `source` against a bannedlist
+
+### Inputs
+- `source`: (String) URL of the source of the webmention
+- `target`: (String) URL of the target of the webmention
+- `acceptableHost`: (String, optional) domains that are accepted by this webmention receiver. Should not include protocols (`https://`). Example: `www.timculverhouse.com`, `sub-domain.domain.tld`
+
+### Outputs
+
+Returns an object with properties:
+- `statusCode`: (Numeric) 400 for rejected webmentions, 200 for verified webmentions
+- `body`: (String) Description of the status code
+- `webmention`: False if rejected, otherwise a [JF2](https://jf2.spec.indieweb.org/) object (see below)
+
+The webmention will be a [JF2](https://jf2.spec.indieweb.org/) object, which will have all of the microformat properties included, as well as:
+- `wm-source`: Equal to `source`
+- `wm-target`: Equal to `target`
+- `wm-property`: Equal to the type of webmention ("in-reply-to","like-of","arbitrary-value")
+
+### Example
--- test/webmention-verifier.test.js
+++ test/webmention-verifier.test.js
@@ -7,31 +7,31 @@ const nock = require('nock');
 describe('webmention-verifier', function() {
   
   it('should return status code 400 if the source URL is not valid', async function() {
-    const expected={statusCode: 400, body: "Source is not a valid URL"};
+    const expected={statusCode: 400, body: "Source is not a valid URL", webmention: false};
     const actual = await verifier('Not a url','https://www.duckduckgo.com');
     assert.deepEqual(actual,expected);
   });
 
   it('should return status code 400 if the target URL is not valid', async function() {
-    const expected={statusCode: 400, body: "Target is not a valid URL"};
+    const expected={statusCode: 400, body: "Target is not a valid URL", webmention: false};
     const actual = await verifier('https://www.duckduckgo.com','Not a url');
     assert.deepEqual(actual,expected);
   });
 
   it('should return status code 400 if the target URL and source URL are the same', async function() {
-    const expected={statusCode: 400,body: "Source and target are same URL"};
+    const expected={statusCode: 400,body: "Source and target are same URL", webmention: false};
     const actual = await verifier('https://www.duckduckgo.com','https://www.duckduckgo.com');
     assert.deepEqual(actual,expected);
   });
 
   it('should return status code 400 if the target domain is not accepted by this receiver', async function() {
-    const expected={statusCode: 400,body: "Target domain is not accepted by this server",};
+    const expected={statusCode: 400,body: "Target domain is not accepted by this server", webmention: false};
     const actual = await verifier('https://www.duckduckgo.com','https://www.google.com','www.duckduckgo.com');
     assert.deepEqual(actual,expected);
   });
 
   it('should return status code 400 if the source url cannot be fetched (does not exist)', async function() {
-    const expected={statusCode: 400,body: "Source url does not exist",};
+    const expected={statusCode: 400,body: "Source url does not exist", webmention: false};
     const actual = await verifier('https://www.thisisavalidurlasdfasdfasd21f25sa1d56fasd.com','https://www.duckduckgo.com');
     assert.deepEqual(actual,expected);
   });
@@ -52,8 +52,8 @@ describe('webmention-verifier', function() {
       .reply(200, data);
 
     const actual = await verifier('https://www.example.com/post.html','https://www.duckduckgo.com');
-    assert.equal(actual["in-reply-to"],"https://www.duckduckgo.com");
-    assert.equal(actual["wm-property"],"in-reply-to");
+    assert.equal(actual.webmention["in-reply-to"],"https://www.duckduckgo.com");
+    assert.equal(actual.webmention["wm-property"],"in-reply-to");
   });
 
   it('should find in-reply-to with multiple h-entries', async function() {
@@ -63,8 +63,8 @@ describe('webmention-verifier', function() {
       .reply(200, data);
 
     const actual = await verifier('https://www.example.com/post.html','https://www.duckduckgo.com');
-    assert.equal(actual["in-reply-to"],"https://www.duckduckgo.com");
-    assert.equal(actual["wm-property"],"in-reply-to");
+    assert.equal(actual.webmention["in-reply-to"],"https://www.duckduckgo.com");
+    assert.equal(actual.webmention["wm-property"],"in-reply-to");
   });
 
    it('should find author as William Shakespeare with only p-author', async function() {
@@ -74,7 +74,7 @@ describe('webmention-verifier', function() {
       .reply(200, data);
 
     const actual = await verifier('https://www.example.com/post.html','https://www.duckduckgo.com');
-    assert.equal(actual.author,"William Shakespeare");
+    assert.equal(actual.webmention.author,"William Shakespeare");
   });
 
    it('should find author as Homer from embedded h-card', async function() {
@@ -84,7 +84,7 @@ describe('webmention-verifier', function() {
       .reply(200, data);
 
     const actual = await verifier('https://www.example.com/post.html','https://www.duckduckgo.com');
-    assert.equal(actual.author.name,"Homer");
+    assert.equal(actual.webmention.author.name,"Homer");
   });
   it('should find author as Pata&ntilde;jali from embedded h-card, not from rel-author page', async function() {
     const data = fs.readFileSync('./test/data/author-with-embedded-h-card-and-rel-author.html');
@@ -93,7 +93,7 @@ describe('webmention-verifier', function() {
       .reply(200, data);
 
     const actual = await verifier('https://www.example.com/post.html','https://www.duckduckgo.com');
-    assert.equal(actual.author.name,"Patañjali");
+    assert.equal(actual.webmention.author.name,"Patañjali");
   });
 
   it('should find author as Virginia Woolf from rel-author page', async function() {
@@ -106,18 +106,18 @@ describe('webmention-verifier', function() {
       .reply(200,data2);
 
     const actual = await verifier('https://www.example.com/post.html','https://www.duckduckgo.com');
-    assert.equal(actual.author.name,"Virginia Woolf");
+    assert.equal(actual.webmention.author.name,"Virginia Woolf");
   });
 
 // LIVE TESTS, MAY BEGIN TO FAIL SOMEDAY
   it('should find author as Webmention Rocks!', async function() {
     const actual = await verifier('https://gregorlove.com/2021/05/i-have-been-using-sublime/','https://www.timculverhouse.com/articles/2021-05-21-text-editors.html');
-    assert.equal(actual.author.name,"gRegor Morrill");
+    assert.equal(actual.webmention.author.name,"gRegor Morrill");
   });
 
   it('should find author as Webmention Rocks!', async function() {
     const actual = await verifier('https://micro.blog/jean/11454935','https://www.timculverhouse.com/notes/2021-05-15-080000.html');
-    assert.equal(actual.author.name,"jean");
+    assert.equal(actual.webmention.author.name,"jean");
   });
 
 });
